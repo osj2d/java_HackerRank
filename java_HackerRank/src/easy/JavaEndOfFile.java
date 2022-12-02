e1d87841c1e87a65ef599eca97c5107852eca05d
osj2d <juniorsavi97@gmail.com>
2022-12-01 23:01:52 -0300

    Exercicios facies

---
+++ java_HackerRank/src/easy/JavaEndOfFile.java
@@ -0,0 +1,7 @@
+package easy;
+
+public class JavaEndOfFile {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java_HackerRank/src/easy/JavaInttoString.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class JavaInttoString {
+    public static void main(String[] args) {
+        int n = 100;
+        String s = String.valueOf(n); //Parte que vai no hackerank
+        System.out.print(s);
+    }
+}
